d2ac41ff331a82613d23258c9ec16b5c33f4131a
sharmistha chakraborty <sharmisthachakraborty2177@gmail.com>
2021-01-14 19:39:16 +0530

    added platform and changed ground color

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill('#603C00');
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
